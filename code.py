e2cad88bd40a70957d6bbd135efbf7b583730aea
Hugoluque-lab <hugo.luqueroyan@students.fhu.edu>
2025-04-29 09:34:01 -0500

    Update code.py

--- code.py
+++ code.py
@@ -72,7 +72,7 @@ class Computer:
     def update_score(self):
         self.score += 1 
 
-# 👉 Esto hace que el juego se inicie
+
 if __name__ == "__main__":
     game = Game()
     game.start_game()
